146099d5e606023b7bb145ac7451a9b50fa94576
Hamza Khalid <sahibzadahamzakhalid@gmail.com>
2025-04-27 09:37:33 +0500

    js updated

--- script.js
+++ script.js
@@ -57,37 +57,3 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         }
     });
 });
-
-// Form submission handling
-
-  const inquiryForm = document.querySelectorAll("inquiry-form");
-
-  inquiryForm.forEach(form => {
-    form.addEventListener("submit", async function(event) {
-      event.preventDefault();
-      const status = form.querySelector(".status"); // 👈 find the status inside this form
-      const data = new FormData(form);
-
-      fetch(form.action, {
-        method: form.method,
-        body: data,
-        headers: { 'Accept': 'application/json' }
-      }).then(response => {
-        if (response.ok) {
-          status.innerHTML = "Thanks for your submission!";
-          form.reset();
-        } else {
-          response.json().then(data => {
-            if (Object.hasOwn(data, 'errors')) {
-              status.innerHTML = data["errors"].map(error => error["message"]).join(", ");
-            } else {
-              status.innerHTML = "Oops! There was a problem submitting your form.";
-            }
-          })
-        }
-      }).catch(error => {
-        status.innerHTML = "Oops! There was a problem submitting your form.";
-      });
-    });
-  });
-
